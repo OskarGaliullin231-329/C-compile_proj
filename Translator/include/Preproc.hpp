d26bb3328e1b8fbe1278a61015fb9f6688c57b0f
OskarGaliullin231-329 <omg197323@gmail.com>
2026-08-15 03:44:11 +0300

    preproc-impl: declared a few private methods for comments deletion and directives execution; started implementing methods for comments deletion

--- Translator/include/Preproc.hpp
+++ Translator/include/Preproc.hpp
@@ -1,32 +1,32 @@
 #ifndef PREPROC_HPP_
 #define PREPROC_HPP_
 
-#include <string>
-#include <vector>
+#include <memory>
 
+// #include "StrIter.hpp"
 #include "../../Loger/include/StdLoger.hpp"
 
 class Preproc final {
 public:
-  enum class SubStrTp : u_int8_t { Comm, Dir, Ord, Err };
-
-  struct SubStr final {
-   std::size_t _begin;
-   std::size_t _end;
-   SubStrTp _tag;
-  };
-
-  Preproc(const std::string& str) { _buff = str; }
+  Preproc(const StdLoger& loger) { _loger = std::make_shared<StdLoger>(loger); }
   Preproc(const Preproc&) = delete;
   Preproc(Preproc&&) = delete;
 
-  bool process();
-  SubStrTp getLineTp(std::size_t index);
+public:
+  bool process(const std::string&);
+  std::string result() { return _text; }
 
 private:
-  std::string_view _buff;
-  std::vector<SubStr> _comms;
-  std::vector<SubStr> _dirs;
+  std::string_view oneLineComment(const std::string_view&);
+  std::string_view multiLineComment(const std::string_view&);
+  std::string_view comment(const std::string_view&);
+  std::string_view directive(const std::string_view&);
+  bool removeComments(const std::string&);
+  bool execDirectives(const std::string&);
+
+private:
+  std::string _text;
+  std::shared_ptr<StdLoger> _loger;
 };
 
 #endif // PREPROC_HPP_
