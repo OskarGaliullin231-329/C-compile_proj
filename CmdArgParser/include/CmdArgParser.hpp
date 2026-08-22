7978aeb87d6889b1b75eae76a84cb684439c954a
OskarGaliullin231-329 <omg197323@gmail.com>
2026-08-23 02:02:33 +0300

    cmd-args-parser: ended implementation of CmdArgParser (only bugs can be found and removed)

--- CmdArgParser/include/CmdArgParser.hpp
+++ CmdArgParser/include/CmdArgParser.hpp
@@ -4,17 +4,29 @@
 #include <map>
 #include <string>
 
+#if DEBUG
+#include <iostream>
+#endif
+
 #include <cstdint>
 
 class CmdArgParser {
 public:
   bool parse(int16_t, char**);
-  int16_t getValAsInt(const std::string& name) {
+  int16_t getAsInt(const std::string& name) {
     return _values[name]._int_val;
   }
-  bool getValAsBool(const std::string& name) {
+  bool getAsBool(const std::string& name) {
     return _values[name]._bool_val;
   }
+#if DEBUG
+  void print() {
+    for (const auto& pair : _values) {
+      std::cout << "name: " << pair.first << '\n';
+      std::cout << "value: " << pair.second._int_val << '\n';
+    }
+  }
+#endif
 
 private:
   union ArgValue {
--- CmdArgParser/src/CmdArgParser.cpp
+++ CmdArgParser/src/CmdArgParser.cpp
@@ -1,6 +1,26 @@
 #include "../include/CmdArgParser.hpp"
 
+#include <iostream>
+
+#include "utils/CmdArgParser_utils.hpp"
+
 bool CmdArgParser::parse(int16_t args_num, char** arg_names) {
-  bool result;
+  bool result = true;
+  for (int16_t i = 1; (i < args_num) && result; i++) {
+    std::string_view str(arg_names[i]);
+    std::string_view name = get_name(str);
+    ArgValue val{ ._bool_val = true };
+    if (name.empty()) {
+      result = false;
+      break;
+    }
+    str.remove_prefix(name.size());
+    if (str.find('=') < str.size()) { val._int_val = get_value(str); }
+    if (val._int_val == 0) {
+      result = false;
+      break;
+    }
+    _values[name] = val;
+  }
   return result;
 }
