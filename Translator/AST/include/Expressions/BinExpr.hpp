f4f4dbe0d7ae7a2b9fcdc63d443a3a7ef3c00e13
OskarGaliullin231-329 <omg197323@gmail.com>
2026-09-03 00:40:29 +0300

    parser-impl: added StructExpr class in BinExpr.hpp file

--- Translator/AST/include/Expressions/BinExpr.hpp
+++ Translator/AST/include/Expressions/BinExpr.hpp
@@ -9,6 +9,7 @@ public:
   BinExpr() = default;
   BinExpr(const BinExpr&) = default;
   BinExpr(const ExprPtr, const ExprPtr, const Token&);
+  ~BinExpr() = default;
 
   TypeId type() override;
 
@@ -21,24 +22,53 @@ protected:
 // <expr> <BIN_OP -> BitOp> <expr>
 class BitExpr final : public BinExpr {
 public:
+  BitExpr() = default;
+  BitExpr(const BitExpr&) = default;
+  BitExpr(const ExprPtr, const ExprPtr, const Token&);
+  ~BitExpr() = default;
+
   TypeId type() override;
 };
 
 // <expr> <BIN_OP -> ArithOp> <expr>
 class ArithExpr final : public BinExpr {
 public:
+  ArithExpr() = default;
+  ArithExpr(const ArithExpr&) = default;
+  ArithExpr(const ExprPtr, const ExprPtr, const Token&);
+  ~ArithExpr() = default;
+
   TypeId type() override;
 };
 
 // <expr> <BIN_OP -> LogicOp> <expr>
 class LgcExpr final : public BinExpr {
 public:
+  LgcExpr() = default;
+  LgcExpr(const LgcExpr&) = default;
+  LgcExpr(const ExprPtr, const ExprPtr, const Token&);
+  ~LgcExpr() = default;
+
   TypeId type() override;
 };
 
 // <ID> <ASGN_OP> <expr>
 class ASGNExpr : public BinExpr {
 public:
+  ASGNExpr() = default;
+  ASGNExpr(const ASGNExpr&) = default;
+  ASGNExpr(const ExprPtr, const ExprPtr, const Token&);
+  ~ASGNExpr() = default;
+
+  TypeId type() override;
+};
+
+class StructExpr : public BinExpr {
+  StructExpr() = default;
+  StructExpr(const StructExpr&) = default;
+  StructExpr(const ExprPtr, const ExprPtr, const Token&);
+  ~StructExpr() = default;
+
   TypeId type() override;
 };
 
