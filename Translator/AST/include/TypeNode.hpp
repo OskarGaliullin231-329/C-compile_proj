1216a0f5e5f399fec873c1bf16b8558c8240dc2b
OskarGaliullin231-329 <omg197323@gmail.com>
2026-09-04 23:48:38 +0300

    parser-impl: added pointer-types support to TypeNode

--- Translator/AST/include/TypeNode.hpp
+++ Translator/AST/include/TypeNode.hpp
@@ -1,18 +1,30 @@
 #ifndef TYPENODE_HPP_
 #define TYPENODE_HPP_
 
+#include <vector>
+
 #include "ASTNode.hpp"
 
-// <TS_KEY || VS_KEY || NONE> <TS_KEY || VS_KEY || NONE> <TP_KEY || TW_KEY>
-// <TS_KEY || VS_KEY || NONE> <TS_KEY || VS_KEY || NONE> <enum || struct>
+// <TS_KEY || VS_KEY || NONE> ... <TP_KEY || TW_KEY || enum || struct || ID> <* || NONE>
 class TypeNode final : public ASTNode {
 public:
+  using Props = std::vector<Token>;
+
+  TypeNode() = default;
+  TypeNode(const TypeNode&) = default;
+  TypeNode(TypeNode&&) = default;
+  ~TypeNode() = default;
+
   TypeId type() override;
 
+  bool setType(const Token&);
+  bool setProp(const Token&);
+  void makePtr();
+
 private:
-  Token _type;      // TP_KEY or TW_KEY
-  Token _tp_spec;   // TS_KEY
-  Token _var_spec;  // VS_KEY
+  Props _props;
+  Token _type;      // TP_KEY, TW_KEY, ID, struct or enum
+  bool _is_ptr = false;
 };
 
 #endif // TYPENODE_HPP_
--- Translator/AST/src/TypeNode.cpp
+++ Translator/AST/src/TypeNode.cpp
@@ -1,3 +1,24 @@
 #include "../include/TypeNode.hpp"
 
 ASTNode::TypeId TypeNode::type() { return ASTNode::type() + 3; }
+
+bool TypeNode::setType(const Token& tok) {
+  switch(tok._tok_tp) {
+    case TokenType::TP_KEY: case TokenType::TW_KEY: case TokenType::ID: { _type = tok; }
+    case TokenType::TD_KEY: {
+      if (!(tok._str != "enum" || tok._str != "struct")) { return false; }
+      _type = tok;
+    } default: { return false; }
+  }
+  return true;
+}
+
+bool TypeNode::setProp(const Token& tok) {
+  switch(tok._tok_tp) {
+    case TokenType::TS_KEY: case TokenType::VS_KEY: { _props.push_back(tok); }
+    default: { return false; }
+  }
+  return true;
+}
+
+void TypeNode::makePtr() { _is_ptr = true; }
